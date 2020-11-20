9413905f21f23ee69562298974b9a5322520b79b
SandraPramod <65229043+SandraPramod@users.noreply.github.com>
2020-11-20 12:59:55 +0530

    Update form1.js

--- form1.js
+++ form1.js
@@ -9,7 +9,7 @@ let error4=document.getElementById("error4");
 let flag=0;
 function validate(){
     let regexp1=/^([A-Za-z0-9\._!#$%&*+/=?^{|}~-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/
-    let regexp2=/^([A-Za-z0-9\._!#$%@&*+/=?^{|}~-]{7,14})$/
+    let regexp2=/^([A-Za-z0-9\._!#$%&*+@/=?^{|}~-]{7,14})$/
     let regexp3=/^([0-9]{10})$/
     let regexp4=/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
         if(regexp1.test(email.value)){
@@ -24,7 +24,7 @@ function validate(){
             error1.style.border='2px solid red';
             flag=0;
             }
-       if(regexp2.test(pwd.value)){
+           if(regexp2.test(pwd.value)){
                 error2.innerHTML='valid password';
                 error2.style.color='green';
                 error2.style.border='2px solid green';
@@ -36,7 +36,7 @@ function validate(){
                 error2.style.border='2px solid red';
                 flag=0;
                 }
-       if(pwd.value==conpwd.value){
+           if(pwd.value==conpwd.value){
             error3.innerHTML='valid';
             error3.style.color='green';
             error3.style.border='2px solid green';
@@ -48,7 +48,7 @@ function validate(){
             error3.style.border='2px solid red';
             flag=0;
             }
-       if((regexp3.test(phno.value))||(regexp4.test(phno.value))){
+           if((regexp3.test(phno.value))||(regexp4.test(phno.value))){
             error4.innerHTML='valid phone number';
             error4.style.color='green';
             error4.style.border='2px solid green';
